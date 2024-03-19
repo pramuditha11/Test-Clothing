c2af2934510b3ed2838e623ee391839fe6095dbf
Pramuditha madushan <75025193+pramuditha11@users.noreply.github.com>
2024-03-19 11:08:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slides = document.querySelectorAll('.slideshow img');
+    let currentSlide = 0;
+
+    setInterval(() => {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].classList.add('active');
+    }, 5000); 
+});
